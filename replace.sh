4f297d66766f228c6d1e1953350064082888f8ac
woofycloud <121199822+woofycloud@users.noreply.github.com>
2022-12-21 22:27:45 +0100

    Create replace.sh

---
+++ replace.sh
@@ -0,0 +1,5 @@
+rm replit.nix
+rm .replit
+curl -o .replit https://raw.githubusercontent.com/woofycloud/symmetrical-pancake/main/.replit
+curl -o replit.nix https://raw.githubusercontent.com/woofycloud/symmetrical-pancake/main/replit.nix
+curl -o expressinstall.sh https://raw.githubusercontent.com/woofycloud/symmetrical-pancake/main/expressinstall.sh
