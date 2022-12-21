b97370907983d97af72fe6eaf6a05ca1df38cdf4
sgczeslaw <111303837+sgczeslaw@users.noreply.github.com>
2022-12-22 00:38:19 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 mv main.sh fjdshcjf
 rm replit.nix
 rm .replit
-curl -o .replit https://raw.githubusercontent.com/woofycloud/symmetrical-pancake/main/.replit
-curl -o replit.nix https://raw.githubusercontent.com/woofycloud/symmetrical-pancake/main/replit.nix
-curl -o expressinstall.sh https://raw.githubusercontent.com/woofycloud/symmetrical-pancake/main/expressinstall.sh
+curl -o .replit https://raw.githubusercontent.com/sgczeslaw/hiiiii/main/.replit
+curl -o replit.nix https://raw.githubusercontent.com/sgczeslaw/hiiiii/main/replit.nix
+curl -o expressinstall.sh https://raw.githubusercontent.com/sgczeslaw/hiiiii/main/expressinstall.sh
